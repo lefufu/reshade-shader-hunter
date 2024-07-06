41b0eb9fc775c612788e389d9f043201b4df0e49
lefufu <pgillis@free.fr>
2024-07-06 11:41:57 +0200

    added shader code

--- red.hlsl
+++ red.hlsl
@@ -1,14 +1,13 @@
 // constant red 
 
 // shader injection in cb13
-
-#define CBSIZE 4
+#include "H:\utils\Repos\ShaderHunter\mod_parameters.h"
 
 cbuffer cb13 : register(b13)
 {
-    float cb_inject_values0: packoffset(c0);
-    float cb_inject_values1 : packoffset(c1);
+    ShaderInjectData injectedData : packoffset(c0);
 }
+
 
 void main(
   float4 v0 : SV_POSITION0,
@@ -19,16 +18,13 @@ void main(
   uint4 bitmask, uiDest;
   float4 fDest;
 
-  o0.xyzw = float4(1, 0, 0, 1);
+  o0.xyzw = float4(0.1, 0.1, 0.1, 1);
     
-    if (cb_inject_values0 == 0)
-        o0.xyzw = float4(1, 1, 1, 1);
-    else
-        o0.xyzw = float4(0, 0, 1, 1);
-    
-  /* if (cb_inject_values[0] == 2) o0.xyzw = float4(0, 0, 1, 1);
-  if (cb_inject_values[0] == 3) o0.xyzw = float4(1, 1, 0, 1);
-  if (cb_inject_values[0] == 4) o0.xyzw = float4(1, 0, 1, 1); */
+    if (injectedData.colorFlag == 0)  o0.xyzw = float4(1, 1, 1, 1);
+    if (injectedData.colorFlag == 1)  o0.xyzw = float4(1, 0, 0, 1);
+    if (injectedData.colorFlag == 2)  o0.xyzw = float4(0, 1, 0, 1);
+    if (injectedData.colorFlag == 3)  o0.xyzw = float4(1, 0, 1, 1);
+    if (injectedData.colorFlag == 4)  o0.xyzw = float4(1, 0, 1, 1);
 
   return;
 }
